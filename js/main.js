0fd34b7e689bb19ae49145a9a1f799f0de2d8d7c
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-03-15 15:26:12 -0300

    Adicionando arquivos iniciais

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length;i++){
+    paciente = pacientes[i];
+    
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
+    
+    var valueImc = paciente.querySelector(".info-imc");
+    
+    var validacao = true;
+    
+    if(peso <= 0 || peso >= 700){
+        console.log("Peso invalido!");
+        valueImc.textContent = "Peso inválido!";
+        validacao = false;
+        paciente.classList.add("paciente-invalido");
+    }
+    if(altura >= 3.00 || altura <= 0.00){
+        console.log("Altura inválida!");
+        valueImc.textContent = "Altura inválida!";
+        validacao = false;
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if(validacao == true){
+        var imc = peso / (altura * altura);
+        valueImc.textContent = imc.toFixed(2);
+    }
+
+}
+
+
+
